732069411ce0585037cd44cfcc3d3723ba6512eb
miguel.rosas <mikelemikelo@gmail.com>
2019-04-15 20:44:16 -0600

    Updating csv_to_json script removing Control M char

--- elasticsearch/input/csv_translator_2_json.sh
+++ elasticsearch/input/csv_translator_2_json.sh
@@ -3,5 +3,5 @@
 source_file=$1
 output_filename=$2
 
-cat $1 | awk -F "," '{print "{ \"index\": { \"_id\":\""$1"_"$5"\"}}\n{ \"business_name\":\""$1"\",\"month_short_name\":\""$2"\",\"day_of_month\":"$3",\"week_day_name\":\""$4"\",\"status_date\":\""$5"\",\"total_pos\":"$6",\"total_cash\":"$7",\"total_cards\":"$8",\"total_expenses\":"$9",\"total_external_expenses\":"$10"}" }' | sed 's/:,/:0.0,/g' | sed 's/:}/:0.0}/g' > ./$2
+cat $1 | awk -F "," '{print "{ \"index\": { \"_id\":\""$1"_"$5"\"}}\n{ \"business_name\":\""$1"\",\"month_short_name\":\""$2"\",\"day_of_month\":"$3",\"week_day_name\":\""$4"\",\"status_date\":\""$5"\",\"total_pos\":"$6",\"total_cash\":"$7",\"total_cards\":"$8",\"total_expenses\":"$9",\"total_external_expenses\":"$10"}" }' | sed -e "s///" |  sed 's/:,/:0.0,/g' | sed 's/:}/:0.0}/g' > ./$2
 
